9eb900b0b047f3d9d228e7983c282aa5614dfaee
Mariana de Cássia Medrano <maremedrano@yahoo.com>
2021-12-09 10:28:27 -0300

    WeHug

---
+++ src/main/java/com/generation/wehug/controller/TemaController.java
@@ -0,0 +1,65 @@
+package com.generation.wehug.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.generation.wehug.model.Tema;
+import com.generation.wehug.repository.TemaRepository;
+
+@RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+@RequestMapping("/tema")
+
+public class TemaController {
+	
+	@Autowired
+	private TemaRepository repository;
+	
+	@GetMapping
+	public ResponseEntity<List<Tema>> getAll(){
+		return ResponseEntity.ok(repository.findAll());
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Tema> getById(@PathVariable long id){
+		return repository.findById(id).map(resp -> ResponseEntity.ok(resp))
+				.orElse(ResponseEntity.notFound().build());
+	}
+	
+	@GetMapping("/topicoPrincipal/{topicoPrincipal}")
+	public ResponseEntity<List<Tema>> getByTopicoPrincipal(@PathVariable String topicoPrincipal){
+		return ResponseEntity.ok(repository.findAllByTopicoPrincipalContainingIgnoreCase(topicoPrincipal));
+	}
+	
+	@PostMapping
+	public ResponseEntity<Tema> post (@RequestBody Tema tema){
+		return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(tema));
+	}
+	
+	@PutMapping
+	public ResponseEntity<Tema> put (@RequestBody Tema tema){
+		return ResponseEntity.ok(repository.save(tema));
+	}	
+	
+	@DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteRepository(@PathVariable long id) {
+        return repository.findById(id).map(resposta -> {
+            repository.deleteById(id);
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+        }).orElse(ResponseEntity.notFound().build());
+    }
+	
+
+}
